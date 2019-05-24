7991575e5d6ac39a19393b3f3ef98cc962bf638a
aparkening <aaron.parkening@gmail.com>
2019-05-23 20:42:48 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,18 @@
-class Student 
+require_relative "./user.rb"
+class Student < User
+
+  def initialize
+    @knowledge = []
+  end
+
+  # Add string to knowledge array
+  def learn(string)
+    @knowledge << string
+  end
+
+  # Return knowledge array
+  def knowledge
+    return @knowledge
+  end
 
 end
