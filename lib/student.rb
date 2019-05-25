c775ab9934d6b7d54504ba43564e7a51225962bb
earthctzn <earthctzn1@gmail.com>
2019-05-25 01:07:28 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
+class Student < User
+attr_accessor :knowledge
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(arg)
+    @knowledge << arg
+  end
+
+  def knowledge
+    @knowledge
+  end 
 
 end
