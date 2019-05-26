5263b355f2bf98bb906d866893a8d5cf0f920565
zhenggao cai <caizhengao1992@gmail.com>
2019-05-26 06:27:16 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,12 @@
-class Student 
-
+class Student < User
+def initialize
+  @knowledge = []
+end
+def learn(new_knowledge)
+  @knowledge << new_knowledge
+end
+def knowledge
+  @knowledge
+end
+  
 end
